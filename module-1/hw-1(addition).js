1ffc4abd0e55976bb69952b9d40c8482fa91d5f3
DmitriyChernobrivects <mezete10@ukr.net>
2018-08-14 19:16:53 +0300

    Module-1

--- module-1/hw-1(addition).js
+++ module-1/hw-1(addition).js
@@ -5,22 +5,22 @@ const taba = 6;
 
 let isConfirmed;
 let seatsCount = +prompt('Enter the number of required seats');
-
-switch (true) {
-    case ((Number.parseInt(seatsCount) !== seatsCount) || (seatsCount <= 0)):
+let validInput = (Number.parseInt(seatsCount) == seatsCount) && (seatsCount > 0);
+switch (validInput) {
+    case false:
         alert('Input Error');
         break;
     case seatsCount <= taba:
         isConfirmed = confirm(`We have seats in group - taba! Would you like to join?`);
-        if (isConfirmed == true)
+        if (isConfirmed)
             break;
     case seatsCount <= sharm:
         isConfirmed = confirm(`We have seats in group - sharm! Would you like to join?`);
-        if (isConfirmed == true)
+        if (isConfirmed)
             break;
     case seatsCount <= hurgada:
         isConfirmed = confirm(`We have seats in group - hurgada! Would you like to join?`);
-        if (isConfirmed == true)
+        if (isConfirmed)
             break;
     default:
         alert('Sorry, there are not so many places in any group!');
