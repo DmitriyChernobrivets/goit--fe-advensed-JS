956f5d17133f6839ec24cb66c9359f05d80c0412
DmitriyChernobrivects <mezete10@ukr.net>
2018-08-19 13:57:04 +0300

    Module-3

--- module-3/js.js
+++ module-3/js.js
@@ -1,28 +1,22 @@
 const logins = ["Mango", "robotGoogles", "Poly", "Aj4x1sBozz", "qwerty123"];
 const userInput = prompt('Введите пароль');
 
-const isLoginValid =  (login) => {
-    return login.length >= 4 && login.length <= 16 ? true : false;
+const isLoginValid =  (login) => login.length >= 4 && login.length <= 16 ? true : false;
+const isLoginUnique = (allLogins, login) => allLogins.includes(login) ? true : false;
+const message = (val) =>  alert(val);
+
+const addLogin =  (login, callback) => {
+    switch (true) {
+        case userInput === null: 
+            return callback('Приходите еще');
+        case !isLoginValid(login):
+            return callback('Ошибка! Логин должен быть от 4 до 16 символов');
+        case !isLoginUnique(logins, login):
+            callback('Логин успешно добавлен!');
+            return logins.push(login);    
+        default:
+            return callback('Такой логин уже используется!');
+    }
 };
 
-const isLoginUnique = (allLogins, login) => {
-    return allLogins.includes(login) ? true : false;
-};
-
-const addLogin =  (login) => {
-        switch (true) {
-            case userInput === null: 
-                return alert('Приходите еще');
-            case !isLoginValid(login):
-                return alert('Ошибка! Логин должен быть от 4 до 16 символов');
-            case !isLoginUnique(logins, login):
-                alert('Логин успешно добавлен!');
-                return logins.push(login);    
-            default:
-                return alert('Такой логин уже используется!');
-        }
-};
-
-addLogin(userInput);
-
-console.log(logins);
+addLogin(userInput, message);
