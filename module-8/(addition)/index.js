f4da844c712bff917628b4da54f45f66aba31ae2
DmitriyChernobrivects <mezete10@ukr.net>
2018-09-12 15:36:53 +0300

    Module-8

--- module-8/(addition)/index.js
+++ module-8/(addition)/index.js
@@ -1,8 +1,19 @@
 'use strict';
+
+const galleryImg = [
+    { preview: 'img/pic-1.jpg', fullview: 'img/pic-1.jpg', alt: "alt text 1" },
+    { preview: 'img/pic-2.jpg', fullview: 'img/pic-2.jpg', alt: "alt text 2" },
+    { preview: 'img/pic-3.jpg', fullview: 'img/pic-3.jpg', alt: "alt text 3" },
+    { preview: 'img/pic-4.jpg', fullview: 'img/pic-4.jpg', alt: "alt text 4" },
+    { preview: 'img/pic-5.jpg', fullview: 'img/pic-5.jpg', alt: "alt text 5" },
+    { preview: 'img/pic-6.jpg', fullview: 'img/pic-6.jpg', alt: "alt text 6" },
+    { preview: 'img/pic-6.jpg', fullview: 'img/pic-6.jpg', alt: "alt text 6" },
+];
+
 class Gallery {
-    constructor(items, parentNode, defaultActiveItem) {
+    constructor(items, parentNodess, defaultActiveItem) {
         this.items = items;
-        this.parentNode = parentNode;
+        this.parentNodess = parentNodess;
         this.defaultActiveItem = defaultActiveItem;
     }
 
@@ -11,12 +22,13 @@ class Gallery {
         const fullview = document.createElement('div');
         const fullviewImg = document.createElement('img');
         const itemList = document.createElement('ul');
-
+        const main = document.createElement('div');
+        main.classList.add('image-gallery', 'js-image-gallery');
         fullview.classList.add('fullview');
-        fullviewImg.classList.add('fullview-preview');
+        fullviewImg.classList.add(this.parentNodess);
         itemList.classList.add('items-view');
-
-        this.parentNode.append(fullview, itemList);
+        document.body.appendChild(main);
+        main.append(fullview, itemList);
         fullview.appendChild(fullviewImg);
         this.items.forEach((el, li, img) => {
             li = document.createElement('li');
@@ -40,50 +52,36 @@ class Gallery {
                 console.log('Нету такого елемента гадереи');
                 return;
             }
-
         };
+
         defaultActiveItem();
 
-        this.parentNode.addEventListener('click', this.galleryEventClick);
+        const galleryEventClick = (event) => {
+            const target = event.target;
+            const imageList = document.querySelectorAll('.items-view_img');
+            const fullviewImg = document.querySelector(`.${this.parentNodess}`);
+            if (((target.nodeName === "IMG") && (target !== fullviewImg))) {
 
-    }
-    galleryEventClick(event) {
-        const target = event.target;
-        const imageList = document.querySelectorAll('.items-view_img');
-        const fullviewImg = document.querySelector('.fullview-preview');
-
-        if (((target.nodeName === "IMG") && (target !== fullviewImg))) {
-            fullviewImg.src = target.dataset.fullview;
-            imageList.forEach(el => {
-                if (el !== target) {
-                    el.classList.remove('modifier')
-                } else
-                    el.classList.add('modifier')
-            })
+                fullviewImg.src = target.dataset.fullview;
+                imageList.forEach(el => {
+                    if (el !== target) {
+                        el.classList.remove('modifier')
+                    } else
+                        el.classList.add('modifier')
+                })
+            }
+            else return;
         }
-        else return;
+
+        main.addEventListener('click', galleryEventClick);
     }
 }
 
-const gallery = new Gallery([
-    { preview: 'img/pic-1.jpg', fullview: 'img/pic-1.jpg', alt: "alt text 1" },
-    { preview: 'img/pic-2.jpg', fullview: 'img/pic-2.jpg', alt: "alt text 2" },
-    { preview: 'img/pic-3.jpg', fullview: 'img/pic-3.jpg', alt: "alt text 3" },
-    { preview: 'img/pic-4.jpg', fullview: 'img/pic-4.jpg', alt: "alt text 4" },
-    { preview: 'img/pic-5.jpg', fullview: 'img/pic-5.jpg', alt: "alt text 5" },
-    { preview: 'img/pic-6.jpg', fullview: 'img/pic-6.jpg', alt: "alt text 6" },
-    { preview: 'img/pic-6.jpg', fullview: 'img/pic-6.jpg', alt: "alt text 6" },
-], document.querySelector('.image-gallery'), 2)
+const gallery = new Gallery(galleryImg, 'fullview-preview', 3);
 
-const gallery2 = new Gallery([
-    { preview: 'img/pic-1.jpg', fullview: 'img/pic-1.jpg', alt: "alt text 1" },
-    { preview: 'img/pic-2.jpg', fullview: 'img/pic-2.jpg', alt: "alt text 2" },
-    { preview: 'img/pic-3.jpg', fullview: 'img/pic-3.jpg', alt: "alt text 3" },
-    { preview: 'img/pic-4.jpg', fullview: 'img/pic-4.jpg', alt: "alt text 4" },
-    { preview: 'img/pic-5.jpg', fullview: 'img/pic-5.jpg', alt: "alt text 5" },
-    { preview: 'img/pic-6.jpg', fullview: 'img/pic-6.jpg', alt: "alt text 6" },
-    { preview: 'img/pic-6.jpg', fullview: 'img/pic-6.jpg', alt: "alt text 6" },
-], document.querySelector('.js-image-gallery2'), 2)
+const gallery2 = new Gallery(galleryImg, 'fullview-preview2', 2);
 
-gallery.galleryRendering()
-gallery2.galleryRendering()
+gallery.galleryRendering();
+gallery2.galleryRendering();
+
+
