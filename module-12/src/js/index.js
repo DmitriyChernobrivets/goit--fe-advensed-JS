5611a001b64c79637b767acb64d06b77ddf8fbf3
DmitriyChernobrivects <mezete10@ukr.net>
2018-10-03 12:12:31 +0300

    asd

---
+++ module-12/src/js/index.js
@@ -0,0 +1,23 @@
+const template = document.querySelector("#card").innerHTML.trim();
+const cardList = document.querySelector(".js-result");
+const btn = document.querySelector(".js-submit");
+
+const fetchApi = () => {
+  return fetch(
+    "http://api.linkpreview.net/?key=123456&q=https://www.google.com"
+  )
+    .then(response => {
+      if (response.ok) return response.json();
+      throw new Error("ERROR FETCH", response.statusText);
+    })
+    .then(data => {
+      console.log(data);
+      //   const markupFunc = Handlebars.compile(template);
+      //   const z = markupFunc(data);
+      //   console.log(z);
+      //   cardList.insertAdjacentHTML("beforeend", z);
+      //   localStorage.setItem("card", JSON.stringify(data));
+    })
+    .catch(err => console.log("err" + err.statusText));
+};
+btn.addEventListener("click", fetchApi);
