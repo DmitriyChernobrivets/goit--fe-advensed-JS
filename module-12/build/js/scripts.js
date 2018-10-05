edebbf76f2905f5a50cc0b8d0596a4b34efdb2dc
DmitriyChernobrivects <mezete10@ukr.net>
2018-10-05 12:51:05 +0300

    module-12

--- module-12/build/js/scripts.js
+++ module-12/build/js/scripts.js
@@ -1,28 +1,90 @@
 "use strict";
 
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
 var template = document.querySelector("#card").innerHTML.trim();
+var errTemplate = document.querySelector("#err").innerHTML.trim();
 var cardList = document.querySelector(".js-result");
 var btn = document.querySelector(".js-submit");
+var input = document.querySelector(".url");
+var errorContainer = document.querySelector(".err-Container");
+var preloader = document.querySelector(".banter-loader");
+var parsedItem = null;
+var err = {
+  error: null
+};
+var pattern = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]{3,25}\.(([a-z]{2,4})\.)?([a-z]{2,4})(\/)?$/; //// ^(https?:\/\/)?(www\.)?[a-zA-Z0-9]{3,25}\.[a-z]{2,4}$
+var localStorageKeys = Object.keys(localStorage);
 
-var fetchApi = function fetchApi() {
-  return fetch(
-    "https://api.linkpreview.net/?key=123456&q=http://www.google.com"
-  )
-    .then(function(response) {
-      if (response.ok) return response.json();
-      throw new Error("ERROR FETCH", response.statusText);
-    })
-    .then(function(data) {
-      console.log(data);
-      //   const markupFunc = Handlebars.compile(template);
-      //   const z = markupFunc(data);
-      //   console.log(z);
-      //   cardList.insertAdjacentHTML("beforeend", z);
-      //   localStorage.setItem("card", JSON.stringify(data));
-    })
-    .catch(function(err) {
-      return console.log("err" + err.statusText);
-    });
+var compileCardTemplate = function compileCardTemplate(obj, template) {
+  var templateFunciton = Handlebars.compile(template);
+  var markup = templateFunciton(obj);
+  cardList.insertAdjacentHTML("afterbegin", markup);
 };
 
-btn.addEventListener("click", fetchApi);
+var errorCompileTemplate = function errorCompileTemplate(obj, template, StringErr) {
+  var templateFunciton = Handlebars.compile(template);
+  obj.error = StringErr;
+  var markup = templateFunciton(obj);
+  errorContainer.innerHTML = markup;
+};
+
+var removeHandler = function removeHandler(storageId) {
+  var removeBtn = document.querySelector(".remove");
+  removeBtn.addEventListener("click", function () {
+    localStorage.removeItem(storageId);
+    removeBtn.parentElement.remove();
+    errorCompileTemplate(err, errTemplate, "Закладка удалена");
+  });
+};
+
+var preloaderToogle = function preloaderToogle() {
+  return preloader.classList.toggle("show");
+};
+
+var isValidInput = function isValidInput() {
+  return pattern.test(input.value) ? true : false;
+};
+
+var onPageLoadedList = function onPageLoadedList() {
+  localStorageKeys.forEach(function (el) {
+    parsedItem = JSON.parse(localStorage.getItem(el));
+    compileCardTemplate(parsedItem, template);
+    removeHandler(parsedItem.title);
+  });
+};
+onPageLoadedList();
+
+var fetchDataProcessing = function fetchDataProcessing(data) {
+  localStorageKeys = [].concat(_toConsumableArray(Object.keys(localStorage)));
+  if (!localStorageKeys.includes(data.title)) {
+    compileCardTemplate(data, template);
+    removeHandler(data.title);
+    localStorage.setItem(data.title, JSON.stringify(data));
+    preloaderToogle();
+  } else {
+    preloaderToogle();
+    return errorCompileTemplate(err, errTemplate, "Такая закладка уже есть");
+  }
+};
+
+var fetchPromise = function fetchPromise(e) {
+  e.preventDefault();
+  preloaderToogle();
+  if (!isValidInput()) {
+    errorCompileTemplate(err, errTemplate, "Ошибка Ввода");
+    preloaderToogle();
+    return;
+  }
+  return fetch("https://api.linkpreview.net/?key=5bb4b458291ae628454d202cf0a8b6e7f2a62a1c36d46&q=" + input.value).then(function (response) {
+    if (response.ok) {
+      return response.json();
+    }
+    errorCompileTemplate(err, errTemplate, "Ошибка Запроса");
+    preloaderToogle();
+  }).then(fetchDataProcessing).catch(function (errr) {
+    return console.log("ERROR", errr);
+  });
+};
+
+btn.addEventListener("click", fetchPromise);
--- module-12/src/js/index.js
+++ module-12/src/js/index.js
@@ -1,24 +1,88 @@
+"use strict";
 const template = document.querySelector("#card").innerHTML.trim();
+const errTemplate = document.querySelector("#err").innerHTML.trim();
 const cardList = document.querySelector(".js-result");
 const btn = document.querySelector(".js-submit");
+const input = document.querySelector(".url");
+const errorContainer = document.querySelector(".err-Container");
+const preloader = document.querySelector(".banter-loader");
+let parsedItem = null;
+let err = {
+  error: null
+};
+const pattern = /^(https?:\/\/)?(www\.)?[a-zA-Z0-9-]{3,25}\.(([a-z]{2,4})\.)?([a-z]{2,4})(\/)?$/; //// ^(https?:\/\/)?(www\.)?[a-zA-Z0-9]{3,25}\.[a-z]{2,4}$
+let localStorageKeys = Object.keys(localStorage);
 
-const fetchApi = () => {
-  return fetch(
-    "https://api.linkpreview.net/?key=123456&q=http://www.google.com"
-  )
-    .then(response => {
-      if (response.ok) return response.json();
-      throw new Error("ERROR FETCH", response.statusText);
-    })
-    .then(data => {
-      console.log(data);
-      //   const markupFunc = Handlebars.compile(template);
-      //   const z = markupFunc(data);
-      //   console.log(z);
-      //   cardList.insertAdjacentHTML("beforeend", z);
-      //   localStorage.setItem("card", JSON.stringify(data));
-    })
-    .catch(err => console.log("err" + err.statusText));
+const compileCardTemplate = (obj, template) => {
+  const templateFunciton = Handlebars.compile(template);
+  const markup = templateFunciton(obj);
+  cardList.insertAdjacentHTML("afterbegin", markup);
 };
 
-btn.addEventListener("click", fetchApi);
+const errorCompileTemplate = (obj, template, StringErr) => {
+  const templateFunciton = Handlebars.compile(template);
+  obj.error = StringErr;
+  const markup = templateFunciton(obj);
+  errorContainer.innerHTML = markup;
+};
+
+const removeHandler = storageId => {
+  const removeBtn = document.querySelector(".remove");
+  removeBtn.addEventListener("click", () => {
+    localStorage.removeItem(storageId);
+    removeBtn.parentElement.remove();
+    errorCompileTemplate(err, errTemplate, "Закладка удалена");
+  });
+};
+
+const preloaderToogle = () => preloader.classList.toggle("show");
+
+const isValidInput = () => (pattern.test(input.value) ? true : false);
+
+const onPageLoadedList = () => {
+  localStorageKeys.forEach(el => {
+    parsedItem = JSON.parse(localStorage.getItem(el));
+    compileCardTemplate(parsedItem, template);
+    removeHandler(parsedItem.title);
+  });
+};
+onPageLoadedList();
+
+const fetchDataProcessing = data => {
+  localStorageKeys = [...Object.keys(localStorage)];
+  if (!localStorageKeys.includes(data.title)) {
+    compileCardTemplate(data, template);
+    removeHandler(data.title);
+    localStorage.setItem(data.title, JSON.stringify(data));
+    preloaderToogle();
+  } else {
+    preloaderToogle();
+    return errorCompileTemplate(err, errTemplate, "Такая закладка уже есть");
+  }
+};
+
+const fetchPromise = e => {
+  e.preventDefault();
+  preloaderToogle();
+  if (!isValidInput()) {
+    errorCompileTemplate(err, errTemplate, "Ошибка Ввода");
+    preloaderToogle();
+    return;
+  }
+  return fetch(
+    `https://api.linkpreview.net/?key=5bb4b458291ae628454d202cf0a8b6e7f2a62a1c36d46&q=${
+      input.value
+    }`
+  )
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      }
+      errorCompileTemplate(err, errTemplate, "Ошибка Запроса");
+      preloaderToogle();
+    })
+    .then(fetchDataProcessing)
+    .catch(errr => console.log("ERROR", errr));
+};
+
+btn.addEventListener("click", fetchPromise);
