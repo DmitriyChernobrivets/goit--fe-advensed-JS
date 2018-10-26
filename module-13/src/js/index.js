8000873da939236f22eb50bd2d8c4682168f9e8f
DmitriyChernobrivects <mezete10@ukr.net>
2018-10-26 18:19:49 +0300

    module-13 #2

--- module-13/src/js/index.js
+++ module-13/src/js/index.js
@@ -2,14 +2,7 @@ import "../scss/styles.scss";
 import View from "../components/View";
 import Model from "../components/Model";
 import Control from "../components/Control";
-import axios from "axios";
-console.log(axios);
-const localStorageKeys = Object.keys(localStorage);
+
 const model = new Model();
 const view = new View();
 new Control(model, view);
-
-// console.log(view);
-const pat = /\ |,|\.|&/g;
-const str = "asdad asd,asda.asdasd& as";
-const res = str.replace(pat, "");
