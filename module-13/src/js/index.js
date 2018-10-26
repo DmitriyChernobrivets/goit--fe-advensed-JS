8c726464e4f6a18da1757cbc0297e3059dc54da7
DmitriyChernobrivects <mezete10@ukr.net>
2018-10-26 18:28:11 +0300

    module-13 #3

--- module-13/src/js/index.js
+++ module-13/src/js/index.js
@@ -2,7 +2,7 @@ import "../scss/styles.scss";
 import View from "../components/View";
 import Model from "../components/Model";
 import Control from "../components/Control";
-
+import axios from "axios";
 const model = new Model();
 const view = new View();
 new Control(model, view);
